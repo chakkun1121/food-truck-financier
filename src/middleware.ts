f2bf42fadd7bdc20a19079d41eca211c522bcd7c
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-04-16 23:17:21 +0000

    feat: make basic authorization

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: ["/(.*)"],
+};
+
+export default function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, password] = atob(authValue).split(":");
+
+    if (user === process.env.USERNAME && password === process.env.PASSWORD) {
+      return NextResponse.next();
+    }
+  }
+
+  return new NextResponse("Unauthorized.", {
+    status: 401,
+    headers: {
+      "WWW-authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
