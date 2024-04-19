3f2d27d8c27aab375999b4f23ef671a43c44e475
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-04-19 04:13:31 +0000

    feat: make register menu ui

--- src/app/register/menu/page.tsx
+++ src/app/register/menu/page.tsx
@@ -2,18 +2,28 @@
 import Loading from "@/components/ui-element/loading";
 import { Button } from "@/components/ui/button";
 import { auth, db } from "@/firebase";
+import { UUID } from "crypto";
 import { ref } from "firebase/database";
 import Link from "next/link";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useObjectVal } from "react-firebase-hooks/database";
+import { Card, CardContent } from "@/components/ui/card";
+import { PlusIcon, MinusIcon } from "@radix-ui/react-icons";
 export default function RegisterPage() {
   const [user, loading, error] = useAuthState(auth);
   const [userInfo, userInfoLoading, userInfoError] = useObjectVal<{
     stallId?: string;
   }>(ref(db, `users/${user?.uid}`));
   const [stallInfo, stallInfoLoading, stallInfoError] = useObjectVal<{
-    name?: string;
+    name: string;
+    commodities?: {
+      [key: UUID]: {
+        name: string;
+        price: number;
+      };
+    };
   }>(ref(db, `stalls/${userInfo?.stallId}`));
+  console.log(stallInfo?.["commodities"]);
   if (loading || userInfoLoading || stallInfoLoading) return <Loading />;
   if (!user || !stallInfo)
     return (
@@ -24,5 +34,41 @@ export default function RegisterPage() {
         </Button>
       </div>
     );
-  return <></>;
+  return (
+    <>
+      <div className="p-4">
+        <h2 className="text-2xl">メニュー</h2>
+        <div className="flex flex-wrap gap-4">
+          {stallInfo?.commodities &&
+            Object.entries(stallInfo?.commodities).map(([key, value]) => (
+              <Card key={key} className="max-w-xs w-full">
+                <CardContent className="p-6 flex justify-between">
+                  <div>
+                    <h3 className="text-xl">{value.name}</h3>
+                    <p className="ml-1">¥{value.price}</p>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <Button
+                      className="rounded-full"
+                      variant="outline"
+                      size="icon"
+                    >
+                      <MinusIcon />
+                    </Button>
+                    <p className="text-lg">0</p>
+                    <Button
+                      className="rounded-full"
+                      variant="outline"
+                      size="icon"
+                    >
+                      <PlusIcon />
+                    </Button>
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
+        </div>
+      </div>
+    </>
+  );
 }
