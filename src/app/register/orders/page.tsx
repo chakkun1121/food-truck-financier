f913026155c9ca9c1912bb9c2d6bee6ee45d5136
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-04-25 17:59:12 +0900

    feat:make  narrow down function in orders page

--- src/app/register/orders/page.tsx
+++ src/app/register/orders/page.tsx
@@ -12,11 +12,19 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { auth, db } from "@/firebase";
 import { OrderType, StallInfo } from "@/types/stallInfo";
 import { CheckIcon, DotsVerticalIcon } from "@radix-ui/react-icons";
 import { UUID } from "crypto";
 import { ref, set } from "firebase/database";
+import { useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useObjectVal } from "react-firebase-hooks/database";
 
@@ -31,6 +39,9 @@ export default function OrdersPage() {
   const [orders, ordersLoading, ordersError] = useObjectVal<{
     [key: UUID]: OrderType;
   }>(ref(db, `stalls/${userInfo?.stallId}/orders`));
+  const [orderStatus, setOrderStatus] = useState<
+    "all" | "pending" | "completed" | "cancelled"
+  >("pending");
   if (
     loading ||
     userInfoLoading ||
@@ -42,24 +53,46 @@ export default function OrdersPage() {
   if (!user || !userInfo?.stallId) return <AccessError />;
   return (
     <>
-      <nav></nav>
+      <div className="flex justify-between p-4">
+        <h2>注文情報</h2>
+        <Select
+          value={orderStatus}
+          onValueChange={(v: any) => setOrderStatus(v)}
+        >
+          <SelectTrigger className="w-24">
+            <SelectValue placeholder="絞り込み" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="all">すべて</SelectItem>
+            <SelectItem value="pending">未完了</SelectItem>
+            <SelectItem value="completed">完了</SelectItem>
+            <SelectItem value="cancelled">キャンセル</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
       <div className="p-4 max-w-7xl mx-auto space-y-4">
-        {Object.entries(orders).map(
-          // @ts-ignore // なぜかidがstringとなるため踏み倒す
-          ([id, order]: [id: UUID, order: OrderType], index) => (
-            <OrderCard
-              key={id}
-              order={{ ...order, id, index }}
-              commodities={commodities}
-              setOrderState={(status: OrderType["status"]) => {
-                set(
-                  ref(db, `stalls/${userInfo.stallId}/orders/${id}/status`),
-                  status
-                );
-              }}
-            />
-          )
-        )}
+        {Object.entries(orders)
+          // @ts-ignore
+          .filter(([_, o]: [_: any, o: OrderType]) => {
+            if (orderStatus == "all") return true;
+            return o.status == orderStatus;
+          })
+          .map(
+            // @ts-ignore // なぜかidがstringとなるため踏み倒す
+            ([id, order]: [id: UUID, order: OrderType], index) => (
+              <OrderCard
+                key={id}
+                order={{ ...order, id, index }}
+                commodities={commodities}
+                setOrderState={(status: OrderType["status"]) => {
+                  set(
+                    ref(db, `stalls/${userInfo.stallId}/orders/${id}/status`),
+                    status
+                  );
+                }}
+              />
+            )
+          )}
       </div>
     </>
   );
