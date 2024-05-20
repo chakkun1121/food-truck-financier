e3790b54d1922f63d12927c4b0773986cf2b9ebe
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-05-21 08:03:16 +0900

    feat:export orders histories (#9)

--- src/app/register/orders/page.tsx
+++ src/app/register/orders/page.tsx
@@ -3,6 +3,13 @@
 import AccessError from "@/components/accessError";
 import OrderCard from "@/components/functional/register/orders/orderCard";
 import Loading from "@/components/ui-element/loading";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
   Select,
   SelectContent,
@@ -11,7 +18,9 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { auth, db } from "@/firebase";
+import { UUIDv7GetTimestamp } from "@/lib/uuidv7-get-timestamp";
 import { OrderType, StallInfo } from "@/types/stallInfo";
+import { DotsVerticalIcon } from "@radix-ui/react-icons";
 import { UUID } from "crypto";
 import { ref, set } from "firebase/database";
 import { useState } from "react";
@@ -33,6 +42,33 @@ export default function OrdersPage() {
   const [orderStatus, setOrderStatus] = useState<
     "all" | "pending" | "ready" | "completed" | "cancelled"
   >("pending");
+  function exportCsv() {
+    // 注文番号 合計金額 注文日時 受取日時 ステータス
+    const csv = [
+      ["注文番号", "合計金額", "注文日時", "受取日時", "ステータス"],
+      // @ts-ignore
+      ...Object.entries(orders)
+        // @ts-ignore
+        .map(([id, order]: [id: UUID, order: OrderType]) =>
+          [
+            order.ticket,
+            Object.values(order.commodities).reduce(
+              (prev, current) => prev + current
+            ),
+            new Date(UUIDv7GetTimestamp(id)).toLocaleString(),
+            "",
+            order.status,
+          ].join(",")
+        ),
+    ].join("\n");
+    const blob = new Blob([csv], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "orders.csv";
+    a.click();
+    URL.revokeObjectURL(url);
+  }
   if (
     loading ||
     userInfoLoading ||
@@ -46,21 +82,35 @@ export default function OrdersPage() {
     <>
       <div className="flex justify-between p-4">
         <h2>注文情報</h2>
-        <Select
-          value={orderStatus}
-          onValueChange={(v: any) => setOrderStatus(v)}
-        >
-          <SelectTrigger className="w-24">
-            <SelectValue placeholder="絞り込み" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="all">すべて</SelectItem>
-            <SelectItem value="pending">未完了</SelectItem>
-            <SelectItem value="ready">準備完了(未受取)</SelectItem>
-            <SelectItem value="completed">完了</SelectItem>
-            <SelectItem value="cancelled">キャンセル</SelectItem>
-          </SelectContent>
-        </Select>
+        <div className="flex">
+          <Select
+            value={orderStatus}
+            onValueChange={(v: any) => setOrderStatus(v)}
+          >
+            <SelectTrigger className="w-24">
+              <SelectValue placeholder="絞り込み" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">すべて</SelectItem>
+              <SelectItem value="pending">未完了</SelectItem>
+              <SelectItem value="ready">準備完了(未受取)</SelectItem>
+              <SelectItem value="completed">完了</SelectItem>
+              <SelectItem value="cancelled">キャンセル</SelectItem>
+            </SelectContent>
+          </Select>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild aria-label="More options">
+              <Button variant="ghost">
+                <DotsVerticalIcon />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem onClick={exportCsv}>
+                エクスポート
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
       <div className="p-4 max-w-7xl mx-auto space-y-4">
         {Object.entries(orders)
