5abf53c5e5b645dd144618ada93b473efdb92cec
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-05-22 03:54:13 +0000

    feat:make link

--- src/components/ui-element/registerHeader.tsx
+++ src/components/ui-element/registerHeader.tsx
@@ -4,6 +4,7 @@ import { ref } from "firebase/database";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useObjectVal } from "react-firebase-hooks/database";
 import Loading from "./loading";
+import { useRouter } from "next/navigation";
 export default function Header() {
   const [user, loading, error] = useAuthState(auth);
   const [userInfo, userInfoLoading, userInfoError] = useObjectVal<{
@@ -12,9 +13,12 @@ export default function Header() {
   const [stallName, stallInfoLoading, stallInfoError] = useObjectVal<string>(
     ref(db, `stalls/${userInfo?.stallId}/name`)
   );
+  const router = useRouter();
   return (
     <header className="flex justify-between p-2 items-center">
-      <h1 className="text-2xl">FoodTruck Financier</h1>
+      <h1 className="text-2xl" onDoubleClick={() => router.push("/")} title="ダブルクリックでトップへ移動">
+        FoodTruck Financier
+      </h1>
       <div>
         {loading || userInfoLoading || stallInfoLoading ? (
           <Loading className="text-start" />
