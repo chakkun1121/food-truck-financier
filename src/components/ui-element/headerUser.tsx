5f9ea32dd9565ff56b79bd7b80547e75fbc7a09d
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-09-05 23:23:18 +0000

    feat:fix ui

--- src/components/ui-element/headerUser.tsx
+++ src/components/ui-element/headerUser.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { auth } from "@/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { Button } from "../ui/button";
@@ -9,8 +9,10 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useError } from "@/hooks/useError";
 export default function HeaderUser() {
   const [user, loading, error] = useAuthState(auth);
+  useError(error);
   return (
     <>
       {user ? (
@@ -21,7 +23,7 @@ export default function HeaderUser() {
               <p>{user?.email}</p>
             </>
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
+          <DropdownMenuContent align="end">
             <DropdownMenuItem>
               <Link href="/user" prefetch={false}>
                 ユーザー情報
