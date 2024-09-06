0c09a15ea144042578c7e7bca8937e24b7b7a6df
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-09-06 23:15:31 +0000

    change:theme color and background color

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -7,11 +7,13 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "fullscreen",
     icons: [
       {
-        src: "https://placehold.jp/3d4070/ffffff/512x512.png?text=Food",
+        src: "https://placehold.jp/fff2e6/000000/512x512.png?text=Food",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
     ],
+    background_color: "#fff2e6",
+    theme_color: "#ff9933",
   };
 }
