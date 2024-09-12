b9d53ca69efb97aef4b8244b10010b5de9103093
chakkun1121 <99847806+chakkun1121@users.noreply.github.com>
2024-09-12 03:32:04 +0000

    refactor

---
+++ src/components/functional/dashboard/export.tsx
@@ -0,0 +1,40 @@
+import { convertCsv } from "@/app/register/orders/convertCsv";
+import { Button } from "@/components/ui/button";
+import { StallInfo } from "@/types/stallInfo";
+import { useState } from "react";
+import { toast } from "sonner";
+// @ts-ignore
+import { zip, dl } from "@/lib/zip";
+
+export default function Export({
+  stalls,
+}: {
+  stalls: { [key: string]: StallInfo };
+}) {
+  const [loading, setLoading] = useState(false);
+  async function download() {
+    try {
+      setLoading(true);
+      const data = Object.entries(stalls).map(([stallId, stall]) => ({
+        id: stallId,
+        name: stall.name,
+        csv: convertCsv(stall?.commodities, stall?.orders!),
+      }));
+      const files = data.map(d => ({
+        name: `${d.name}.csv`,
+        buffer: new TextEncoder().encode(d.csv),
+      }));
+      dl({ name: "stalls.zip", buffer: await zip(files) });
+    } catch (e: any) {
+      console.error(e);
+      toast.error(`エクスポートに失敗しました: ${e.message}`);
+    } finally {
+      setLoading(false);
+    }
+  }
+  return (
+    <Button onClick={() => download()} disabled={loading}>
+      データをエクスポート
+    </Button>
+  );
+}
--- src/components/functional/dashboard/index.tsx
+++ src/components/functional/dashboard/index.tsx
@@ -1,65 +1,27 @@
 "use client";
 
 import Loading from "@/app/loading";
-import { auth, db } from "@/firebase";
+import { db } from "@/firebase";
 import { useError } from "@/hooks/useError";
 import { StallInfo } from "@/types/stallInfo";
 import { ref } from "firebase/database";
-import { useAuthState } from "react-firebase-hooks/auth";
 import { useObjectVal } from "react-firebase-hooks/database";
 import { SalesSheet } from "./salesSheet";
 import { StockSheet } from "./stockSheet";
-import { Button } from "@/components/ui/button";
-import { convertCsv } from "@/app/register/orders/convertCsv";
-// @ts-ignore
-import { zip, dl } from "@/lib/zip";
-import { useState } from "react";
-import { toast } from "sonner";
+import Export from "./export";
 
 export default function Dashboard() {
-  const [user, loading, error] = useAuthState(auth);
   const [stalls, stallsLoading, stallsError] = useObjectVal<{
     [key: string]: StallInfo;
   }>(ref(db, "stalls"));
-  useError(error, stallsError);
-  if (loading || stallsLoading) return <Loading />;
+  useError(stallsError);
+  if (stallsLoading) return <Loading />;
   return (
     <>
       <h1 className="text-2xl">ダッシュボード</h1>
       <SalesSheet stalls={stalls!} />
-
-      {/* 在庫が少ない商品リスト */}
       <StockSheet stalls={stalls!} />
-      {/* 放置されている注文 */}
       <Export stalls={stalls!} />
     </>
-  );
-}
-function Export({ stalls }: { stalls: { [key: string]: StallInfo } }) {
-  const [loading, setLoading] = useState(false);
-  async function download() {
-    try {
-      setLoading(true);
-      const data = Object.entries(stalls).map(([stallId, stall]) => ({
-        id: stallId,
-        name: stall.name,
-        csv: convertCsv(stall?.commodities, stall?.orders!),
-      }));
-      const files = data.map(d => ({
-        name: `${d.name}.csv`,
-        buffer: new TextEncoder().encode(d.csv),
-      }));
-      dl({ name: "stalls.zip", buffer: await zip(files) });
-    } catch (e: any) {
-      console.error(e);
-      toast.error(`エクスポートに失敗しました: ${e.message}`);
-    } finally {
-      setLoading(false);
-    }
-  }
-  return (
-    <Button onClick={() => download()} disabled={loading}>
-      データをエクスポート
-    </Button>
   );
 }
