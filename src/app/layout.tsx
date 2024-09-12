cef0fe165681c975ec4b77d0b31a71707ff88f2a
gendaineko2222 <gendaineko2222@gmail.com>
2024-09-13 00:09:28 +0900

    feat: フォントを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,54 +1,74 @@
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/sonner";
-import "@/lib/customErrorMap";
-import { cn } from "@/lib/utils";
-import firebase from "firebase/compat/app";
-import type { Metadata, Viewport } from "next";
-import { Noto_Sans_JP } from "next/font/google";
-import "./globals.css";
-const noto = Noto_Sans_JP({
-  weight: ["400", "700"],
-  style: "normal",
-  subsets: ["latin"]
-});
+import type { Metadata, Viewport } from 'next'
+import { Noto_Sans_JP, Poppins } from 'next/font/google'
+
+import { ThemeProvider } from '@/components/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
+import { cn } from '@/lib/utils'
+import firebase from 'firebase/compat/app'
+
+import '@/lib/customErrorMap'
+
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  adjustFontFallback: false,
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+})
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+  display: 'swap',
+  adjustFontFallback: false,
+  variable: '--font-noto-sans-jp',
+})
 
 export const metadata: Metadata = {
   title: {
-    default: "FoodTruck Financier",
-    template: "%s | FoodTruck Financier"
+    default: 'FoodTruck Financier',
+    template: '%s | FoodTruck Financier',
   },
   robots: {
-    index: false
-  }
-};
+    index: false,
+  },
+}
 export const viewport: Viewport = {
-  themeColor: "#ff9933"
-};
+  themeColor: '#ff9933',
+}
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-  if (typeof window !== "undefined") {
-    window.addEventListener("beforeunload", () => {
-      console.log("Disconnecting from Firebase");
-      firebase.database().goOffline();
-    });
+  if (typeof window !== 'undefined') {
+    window.addEventListener('beforeunload', () => {
+      // biome-ignore lint/suspicious/noConsoleLog: <explanation>
+      console.log('Disconnecting from Firebase')
+      firebase.database().goOffline()
+    })
   }
+
   return (
-    <html lang="ja">
-      <body className={cn("min-h-screen select-none", noto.className)}>
+    <html lang='ja'>
+      <body
+        className={cn(
+          'select-none min-h-screen',
+          `${poppins.variable} ${notoSansJP.variable} font-sans`,
+        )}
+      >
         <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
+          attribute='class'
+          defaultTheme='light'
+          enableSystem={true}
+          disableTransitionOnChange={true}
         >
           {children}
           <Toaster />
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
