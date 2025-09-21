11f926166eafcc688ea9aec07974194fbd5e095e
Kajitaka <142304219+kajitaka1201@users.noreply.github.com>
2025-09-21 08:10:34 +0000

    firebase admin apiでユーザー作成する機能の追加

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,5 +1,3 @@
-import "server-only";
-
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { getDatabase } from "firebase-admin/database";
@@ -7,9 +5,9 @@ import { getDatabase } from "firebase-admin/database";
 if (!getApps()?.length) {
   initializeApp({
     credential: cert(
-      JSON.parse(JSON.stringify(process.env.FIREBASE_SERVICE_ACCOUNT_KEY))
+      JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}")
     ),
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL
   });
 }
 
